1a8cc92d97a45855903be3c11b0cd3544d8a33e0
evan ciok <eciok@stevens.edu>
2024-11-23 19:38:13 -0500

    updates to front end styles

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -1,21 +1,42 @@
 import React from 'react';
+import rainDropIcon from './assets/rain-drop-icon.png';
 
 function DisplayWeather({ weatherData }) {
   if (!weatherData) {
     return <p>No data available for the input location. Please try another.</p>;
   }
 
+  const formatCoordinates = (coords) => {
+    const [lat, lon] = coords.split(', ').map(Number);
+    const latDirection = lat >= 0 ? 'N' : 'S';
+    const lonDirection = lon >= 0 ? 'E' : 'W';
+    const formattedLat = Math.abs(lat).toFixed(2);
+    const formattedLon = Math.abs(lon).toFixed(2);
+    return `${formattedLat}° ${latDirection}, ${formattedLon}° ${lonDirection}`;
+  };
+
+  const formatDateTime = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    return date.toLocaleString(undefined, options);
+  };
+
   return (
     <div className="weather-container">
-      <h2>Current Weather</h2>
-      <p>Location: {weatherData.location ? weatherData.location : 'Not available'}</p>
-      <p>Coordinates: {weatherData.coordinates}</p>
-      <p>Time: {new Date(weatherData.time * 1000).toLocaleString()}</p>
-      <p>Temperature: {weatherData.temperature}°F</p>
-      <p>Feels Like: {weatherData.feels_like}°F</p>
-      <p>Humidity: {weatherData.humidity}%</p>
-      <p>Condition: {weatherData.main}</p>
-      <p>Description: {weatherData.description}</p>
+      <h2>{weatherData.location || 'Unknown Location'}</h2>
+      <p className="subtext">{formatCoordinates(weatherData.coordinates)}</p>
+      <p className="datetime">{formatDateTime(weatherData.time)}</p>
+      
+      <div className="temperature-container">
+        <span className="temp">{Math.round(weatherData.temperature)}°F</span>
+        <span className="feels-like">({Math.round(weatherData.feels_like)}°F)</span>
+      </div>
+      <p className="humidity">
+        <img src={rainDropIcon} alt="humidity icon" className="humidity-icon" />
+        {weatherData.humidity}%
+      </p>
+      <p className="description">{weatherData.description}</p>
+      
     </div>
   );
 }
